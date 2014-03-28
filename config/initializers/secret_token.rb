e57f45725d862adba5ec9fe9314536abf2360e82
Carlos Narez <cnarez@gmail.com>
2014-03-27 22:04:21 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Carlosnarez::Application.config.secret_key_base = '5288bdfe8d6855d3443f7fc46f721cc63737d2ebcac06099afdbdf4a08f21c2a6265a25b3fadc774db4a75ccec20244819b976c3804df74709b501e9b1fbf2c9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Carlosnarez::Application.config.session_store :cookie_store, key: '_carlosnarez_session'
